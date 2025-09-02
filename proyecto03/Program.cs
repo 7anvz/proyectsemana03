89e29929d4b97d0077607ae4ff75eb659dd99517
7anvz <danjscommunity@gmail.com>
2025-09-02 11:56:50 -0500

    Repositorio local a remoto

---
+++ proyecto03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+        }
+        static void ejer1()
+        {
+            string name, carrera;
+            Console.WriteLine("FORMULARIO A COMPLETAR:");
+            Console.WriteLine("----------------------------");
+            Console.Write(">> Ingrese su nombre: ");
+            name = Console.ReadLine();
+            Console.Write(">> Ingrese su carrera: ");
+            carrera = Console.ReadLine();
+            Console.WriteLine("----------------------------");
+            Console.Write($">>> {name} Bienvenido a Mundo UPN en su carrera de: {carrera} ");
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ proyectpython/proyectpython.py
@@ -0,0 +1,9 @@
+def ejer1():
+    print("FORMULARIO A COMPLETAR:");
+    print("-----------------------------");
+    name = input(">> Ingrese su nombre: ");
+    carrera = input(">> Ingrese su nombre: ");
+    print("-----------------------------");
+    print(f">>> {name} Bienvenido a Mundo UPN en su carrera de: {carrera} ");
+
+ejer1();
